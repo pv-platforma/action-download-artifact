c80388378bac66a858d79c0147db29a4867f9667
Juanma Hidalgo <juanma06@gmail.com>
2021-08-03 18:05:33 +0200

    Adds some console.logs

--- main.js
+++ main.js
@@ -1,138 +1,143 @@
-const core = require('@actions/core')
-const github = require('@actions/github')
-const AdmZip = require('adm-zip')
-const filesize = require('filesize')
-const pathname = require('path')
-const fs = require('fs')
+const core = require("@actions/core");
+const github = require("@actions/github");
+const AdmZip = require("adm-zip");
+const filesize = require("filesize");
+const pathname = require("path");
+const fs = require("fs");
 
 async function main() {
-    try {
-        const token = core.getInput("github_token", { required: true })
-        const workflow = core.getInput("workflow", { required: true })
-        const [owner, repo] = core.getInput("repo", { required: true }).split("/")
-        const path = core.getInput("path", { required: true })
-        const name = core.getInput("name")
-        let workflowConclusion = core.getInput("workflow_conclusion")
-        let pr = core.getInput("pr")
-        let commit = core.getInput("commit")
-        let branch = core.getInput("branch")
-        let event = core.getInput("event")
-        let runID = core.getInput("run_id")
-        let runNumber = core.getInput("run_number")
+  try {
+    const token = core.getInput("github_token", { required: true });
+    const workflow = core.getInput("workflow", { required: true });
+    const [owner, repo] = core.getInput("repo", { required: true }).split("/");
+    const path = core.getInput("path", { required: true });
+    const name = core.getInput("name");
+    let workflowConclusion = core.getInput("workflow_conclusion");
+    let pr = core.getInput("pr");
+    let commit = core.getInput("commit");
+    let branch = core.getInput("branch");
+    let event = core.getInput("event");
+    let runID = core.getInput("run_id");
+    let runNumber = core.getInput("run_number");
 
-        const client = github.getOctokit(token)
+    const client = github.getOctokit(token);
 
-        console.log("==> Workflow:", workflow)
+    console.log("==> Workflow:", workflow);
 
-        console.log("==> Repo:", owner + "/" + repo)
+    console.log("==> Repo:", owner + "/" + repo);
 
-        console.log("==> Conclusion:", workflowConclusion)
+    console.log("==> Conclusion:", workflowConclusion);
 
-        if (pr) {
-            console.log("==> PR:", pr)
+    if (pr) {
+      console.log("==> PR:", pr);
 
-            const pull = await client.pulls.get({
-                owner: owner,
-                repo: repo,
-                pull_number: pr,
-            })
-            commit = pull.data.head.sha
-        }
-
-        if (commit) {
-            console.log("==> Commit:", commit)
-        }
-
-        if (branch) {
-            branch = branch.replace(/^refs\/heads\//, "")
-            console.log("==> Branch:", branch)
-        }
-
-        if (event) {
-            console.log("==> Event:", event)
-        }
-
-        if (runNumber) {
-            console.log("==> RunNumber:", runNumber)
-        }
-
-        if (!runID) {
-            for await (const runs of client.paginate.iterator(client.rest.actions.listWorkflowRuns, {
-                owner: owner,
-                repo: repo,
-                workflow_id: workflow,
-                branch: branch,
-                event: event,
-                status: workflowConclusion,
-            }
-            )) {
-                const run = runs.data.find(r => {
-                    if (commit) {
-                        return r.head_sha == commit
-                    }
-                    if (runNumber) {
-                        return r.run_number == runNumber
-                    }
-                    return true
-                })
-
-                if (run) {
-                    runID = run.id
-                    break
-                }
-            }
-        }
-
-        console.log("==> RunID:", runID)
-
-        let artifacts = await client.paginate(client.rest.actions.listWorkflowRunArtifacts, {
-            owner: owner,
-            repo: repo,
-            run_id: runID,
-        })
-
-        // One artifact or all if `name` input is not specified.
-        if (name) {
-            artifacts = artifacts.filter((artifact) => {
-                return artifact.name == name
-            })
-        }
-
-        if (artifacts.length == 0)
-            throw new Error("no artifacts found")
-
-        for (const artifact of artifacts) {
-            console.log("==> Artifact:", artifact.id)
-
-            const size = filesize(artifact.size_in_bytes, { base: 10 })
-
-            console.log(`==> Downloading: ${artifact.name}.zip (${size})`)
-
-            const zip = await client.actions.downloadArtifact({
-                owner: owner,
-                repo: repo,
-                artifact_id: artifact.id,
-                archive_format: "zip",
-            })
-
-            const dir = name ? path : pathname.join(path, artifact.name)
-
-            fs.mkdirSync(dir, { recursive: true })
-
-            const adm = new AdmZip(Buffer.from(zip.data))
-
-            adm.getEntries().forEach((entry) => {
-                const action = entry.isDirectory ? "creating" : "inflating"
-                const filepath = pathname.join(dir, entry.entryName)
-
-                console.log(`  ${action}: ${filepath}`)
-            })
-
-            adm.extractAllTo(dir, true)
-        }
-    } catch (error) {
-        core.setFailed(error.message)
+      const pull = await client.pulls.get({
+        owner: owner,
+        repo: repo,
+        pull_number: pr,
+      });
+      commit = pull.data.head.sha;
     }
+
+    if (commit) {
+      console.log("==> Commit:", commit);
+    }
+
+    if (branch) {
+      branch = branch.replace(/^refs\/heads\//, "");
+      console.log("==> Branch:", branch);
+    }
+
+    if (event) {
+      console.log("==> Event:", event);
+    }
+
+    if (runNumber) {
+      console.log("==> RunNumber:", runNumber);
+    }
+
+    if (!runID) {
+      for await (const runs of client.paginate.iterator(
+        client.rest.actions.listWorkflowRuns,
+        {
+          owner: owner,
+          repo: repo,
+          workflow_id: workflow,
+          branch: branch,
+          event: event,
+          status: workflowConclusion,
+        }
+      )) {
+        console.log("runs: ", runs);
+        const run = runs.data.find((r) => {
+          if (commit) {
+            return r.head_sha == commit;
+          }
+          if (runNumber) {
+            return r.run_number == runNumber;
+          }
+          return true;
+        });
+
+        if (run) {
+          runID = run.id;
+          break;
+        }
+      }
+    }
+
+    console.log("==> RunID:", runID);
+
+    let artifacts = await client.paginate(
+      client.rest.actions.listWorkflowRunArtifacts,
+      {
+        owner: owner,
+        repo: repo,
+        run_id: runID,
+      }
+    );
+
+    // One artifact or all if `name` input is not specified.
+    if (name) {
+      artifacts = artifacts.filter((artifact) => {
+        return artifact.name == name;
+      });
+    }
+
+    if (artifacts.length == 0) throw new Error("no artifacts found");
+
+    for (const artifact of artifacts) {
+      console.log("==> Artifact:", artifact.id);
+
+      const size = filesize(artifact.size_in_bytes, { base: 10 });
+
+      console.log(`==> Downloading: ${artifact.name}.zip (${size})`);
+
+      const zip = await client.actions.downloadArtifact({
+        owner: owner,
+        repo: repo,
+        artifact_id: artifact.id,
+        archive_format: "zip",
+      });
+
+      const dir = name ? path : pathname.join(path, artifact.name);
+
+      fs.mkdirSync(dir, { recursive: true });
+
+      const adm = new AdmZip(Buffer.from(zip.data));
+
+      adm.getEntries().forEach((entry) => {
+        const action = entry.isDirectory ? "creating" : "inflating";
+        const filepath = pathname.join(dir, entry.entryName);
+
+        console.log(`  ${action}: ${filepath}`);
+      });
+
+      adm.extractAllTo(dir, true);
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
-main()
+main();
